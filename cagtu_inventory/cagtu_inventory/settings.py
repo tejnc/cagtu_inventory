30d0f166eb382fd2140e8cde3a52340199c9113c
tejnc <tejnarayan15.tnc@gmail.com>
2022-05-29 11:44:12 +0545

    simple custom auth using django auth

--- cagtu_inventory/cagtu_inventory/settings.py
+++ cagtu_inventory/cagtu_inventory/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cagtu_inventory.wsgi.application'
+
+AUTH_USER_MODEL = "users.Users"
 
 
 # Database
--- cagtu_inventory/users/models.py
+++ cagtu_inventory/users/models.py
@@ -1,30 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
 
 # Create your models here.
 class UserManager(BaseUserManager):
-    def create_user(self, email,company_name, phone_number, password=None):
+    def create_user(self, email,company_name, phone, password=None):
         if not email:
             raise ValueError("email is required")
         if not company_name:
             raise ValueError("company name is required")
-        if not phone_number:
+        if not phone:
             raise ValueError("prepare provide an active phone number")
         
         user = self.model(
             email = self.normalize_email(email),
             company_name = company_name,
-            phone = phone_number
+            phone = phone
         )
         user.set_password(password)
         user.save(using=self._db)
         return user 
+    
+    def create_superuser(self, email, company_name, phone , password=None):
+        user = self.create_user(
+            email= email,
+            phone =phone,
+            password = password,
+            company_name=company_name
+        )
+        user.is_admin = True 
+        user.is_staff = True
+        user_is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class Users(AbstractBaseUser):
     email = models.EmailField(verbose_name="email address", max_length=60, unique=True)
     company_name = models.CharField(verbose_name="company name",max_length=60)
-    phone_number = models.CharField(max_length=20, verbose_name="phone number")
+    phone = models.CharField(max_length=20, verbose_name="phone number")
     date_joined = models.DateTimeField(verbose_name= "date joined",auto_now_add=True)
     last_login = models.DateTimeField(verbose_name="last login", auto_now=True )
     is_admin = models.BooleanField(default=False)
@@ -33,7 +46,9 @@ class Users(AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["company_name","phone_number",]
+    REQUIRED_FIELDS = ["company_name","phone"]
+
+    objects = UserManager()
 
     def __str__(self):
         return self.company_name
